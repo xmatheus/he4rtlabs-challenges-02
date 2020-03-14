8c674efd82f950e166e9889c1ed0749690fb8696
Daniel He4rt <idanielreiss@gmail.com>
2020-03-13 21:07:46 -0300

    agora vai

---
+++ src/assets/js/main.js
@@ -0,0 +1,11 @@
+// Este será o objeto principal no qual você irá se basear para as funções do desafio!
+// Caso haja dúvidas de como prosseguir, favor consultar a sala #js no nosso discord!
+let features = [
+  {
+    feature: "Authentication",
+    devHours: 10,
+    testHours: 2
+  }
+];
+
+// Dica: faça o layout e depois pense em como vai funcionar o script.
